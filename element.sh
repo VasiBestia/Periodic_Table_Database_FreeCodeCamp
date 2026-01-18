073ad04252217155b8beda37a12f1ebf13ede1a7
Vasilescu Alexandru Gabriel <162467637+VasiBestia@users.noreply.github.com>
2026-01-18 11:30:14 -0500

    feat:Adding condition for passing argument

--- element.sh
+++ element.sh
@@ -6,9 +6,25 @@ if [[ -z $1 ]]
     then 
     echo Please provide an element as an argument.
     else
-    RETURN_RESULT=$($PSQL "Select e.atomic_number,e.name,e.symbol,p.type,p.atomic_mass,
-                          p.melting_point_celsius,p.boiling_point_celsius 
-                          From elements e Join properties p on p.atomic_number=e.atomic_number
-                          Where e.atomic_number=$1");
-    echo $RETURN_RESULT                      
+    if [[ $1 =~ ^[0-9]+$ ]]
+        then
+        RETURN_RESULT=$($PSQL "Select e.atomic_number,e.name,e.symbol,p.type,p.atomic_mass,
+                              p.melting_point_celsius,p.boiling_point_celsius 
+                              From elements e Join properties p on p.atomic_number=e.atomic_number
+                              Where e.atomic_number=$1");
+        echo $RETURN_RESULT
+        elif [[ ${#1} -le 2 ]]
+          then
+          RETURN_RESULT=$($PSQL "Select e.atomic_number,e.name,e.symbol,p.type,p.atomic_mass,
+                              p.melting_point_celsius,p.boiling_point_celsius 
+                              From elements e Join properties p on p.atomic_number=e.atomic_number
+                              Where e.symbol='$1'");
+        echo $RETURN_RESULT
+        else
+        RETURN_RESULT=$($PSQL "Select e.atomic_number,e.name,e.symbol,p.type,p.atomic_mass,
+                              p.melting_point_celsius,p.boiling_point_celsius 
+                              From elements e Join properties p on p.atomic_number=e.atomic_number
+                              Where e.name='$1'");
+        echo $RETURN_RESULT
+        fi                      
     fi
