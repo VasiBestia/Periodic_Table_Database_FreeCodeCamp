c4cd116eb2ae8875d291b020193944db652b4359
Vasilescu Alexandru Gabriel <162467637+VasiBestia@users.noreply.github.com>
2026-01-18 11:36:50 -0500

    test:Adding output format

--- element.sh
+++ element.sh
@@ -12,19 +12,28 @@ if [[ -z $1 ]]
                               p.melting_point_celsius,p.boiling_point_celsius 
                               From elements e Join properties p on p.atomic_number=e.atomic_number
                               Where e.atomic_number=$1");
-        echo $RETURN_RESULT
+        echo $RETURN_RESULT|while IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+        do 
+          echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+        done 
         elif [[ ${#1} -le 2 ]]
           then
           RETURN_RESULT=$($PSQL "Select e.atomic_number,e.name,e.symbol,p.type,p.atomic_mass,
                               p.melting_point_celsius,p.boiling_point_celsius 
                               From elements e Join properties p on p.atomic_number=e.atomic_number
                               Where e.symbol='$1'");
-        echo $RETURN_RESULT
+        echo $RETURN_RESULT|while IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+        do 
+          echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+        done 
         else
         RETURN_RESULT=$($PSQL "Select e.atomic_number,e.name,e.symbol,p.type,p.atomic_mass,
                               p.melting_point_celsius,p.boiling_point_celsius 
                               From elements e Join properties p on p.atomic_number=e.atomic_number
                               Where e.name='$1'");
-        echo $RETURN_RESULT
+        echo $RETURN_RESULT|while IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+        do 
+          echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+        done 
         fi                      
     fi
