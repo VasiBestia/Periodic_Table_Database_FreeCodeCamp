088d7f4ad59a8a6b5b55fcd352a635a0ef2daf21
Vasilescu Alexandru Gabriel <162467637+VasiBestia@users.noreply.github.com>
2026-01-18 11:18:22 -0500

    feat:Adding Psql functionality+first review

--- element.sh
+++ element.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [[ -z $1 ]]
     then 
     echo Please provide an element as an argument.
     else
-    echo E gol
+    RETURN_RESULT=$($PSQL "Select e.atomic_number,e.name,e.symbol,p.type,p.atomic_mass,
+                          p.melting_point_celsius,p.boiling_point_celsius 
+                          From elements e Join properties p on p.atomic_number=e.atomic_number
+                          Where e.atomic_number=$1");
+    echo $RETURN_RESULT                      
     fi
